5df5ab27b0ff9b3e710edf978defb7b74bd11643
Anders Urrang <aurrang@gmail.com>
2024-06-17 12:59:09 +0200

    fix formatting errors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		console.log('Hello world');
+	});
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
